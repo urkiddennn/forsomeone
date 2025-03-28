a613f573c2203f384f27b524f829ed09b6cf2ba1
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-03-28 17:42:56 +0800

    dsds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "src"), // Alias for shorter imports
-        },
-      },
+
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      // You can remove 'external' unless you specifically need these as externals
-      external: [],
-    },
-  },
+
 });
